da409b2f18074963d49db66a6fcc8a2129d8d854
Alexion <alex24bryant@mail.ru>
2022-09-23 18:22:43 +0500

    add administration info endpoint

--- shortener_app/main.py
+++ shortener_app/main.py
@@ -2,9 +2,12 @@ import validators
 from fastapi import FastAPI, HTTPException, Depends, Request
 from fastapi.responses import RedirectResponse
 from sqlalchemy.orm import Session
+from starlette.datastructures import URL
 
 from . import schemas, models, crud
 from .database import SessionLocal, engine
+from .config import get_settings
+
 
 
 app = FastAPI()
@@ -39,10 +42,7 @@ def create_url(url: schemas.URLBase, db: Session = Depends(get_db)):
         raise_bad_request(message='Your provided URL is not valid')
 
     db_url = crud.create_db_url(db=db, url=url)
-    db_url.url = db_url.key
-    db_url.admin_url = db_url.secret_key
-
-    return db_url
+    return get_admin_info(db_url)
 
 
 @app.get('/{url_key}')
@@ -56,3 +56,27 @@ def forward_to_target_url(
         return RedirectResponse(db_url.target_url)
     else:
         raise_not_found(request)
+
+
+@app.get(
+    "/admin/{secret_key}",
+    name="administration info",
+    response_model=schemas.URLInfo
+)
+def get_url_info(
+        secret_key: str, request: Request, db: Session = Depends(get_db)
+):
+    if db_url := crud.get_db_url_by_secret_key(db, secret_key=secret_key):
+        return get_admin_info(db_url)
+    else:
+        raise_not_found(request)
+
+
+def get_admin_info(db_url: models.URL) -> schemas.URLInfo:
+    base_url = URL(get_settings().base_url)
+    admin_endpoint = app.url_path_for(
+        "administration info", secret_key=db_url.secret_key
+    )
+    db_url.url = str(base_url.replace(path=db_url.key))
+    db_url.admin_url = str(base_url.replace(path=admin_endpoint))
+    return db_url
